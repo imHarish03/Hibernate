bfd87284d457d321d88bb4f006e03f745fcc3d68
imHarish03 <harishhari302@gmail.com>
2019-11-02 22:32:04 +0530

    * Code optimized, factory object closed

--- src/main/java/lab/basic/Hibernate/App.java
+++ src/main/java/lab/basic/Hibernate/App.java
@@ -7,18 +7,30 @@ import org.hibernate.cfg.Configuration;
 
 public class App {
 	public static void main(String[] args) {
-		try {
-			SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class)
-					.buildSessionFactory();
+		SessionFactory factory = new Configuration()
+				// Hibernate will load default file
+				.configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
 
+		try {
 			Session session = factory.getCurrentSession();
+			// Create a User Object
+			User user = new User("Christopher", "Krishnagiri");
+
+			// Start transaction
 			Transaction tx = session.beginTransaction();
+
+			// Save the transaction
+			session.save(user);
+
 			User getUser = (User) session.get(User.class, 1);
+			System.out.println("**Get Data***");
 			System.out.println("Username is :" + getUser.getName());
 			tx.commit();
 
 		} catch (Exception e) {
 			System.out.println(e);
+		} finally {
+			factory.close();
 		}
 	}
 }
--- src/main/java/lab/basic/Hibernate/User.java
+++ src/main/java/lab/basic/Hibernate/User.java
@@ -3,20 +3,32 @@ package lab.basic.Hibernate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "person")
 public class User {
 	@Id
-	@GeneratedValue
 	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "empId")
+	//@SequenceGenerator(name="empId",initialValue = 100, allocationSize = 2, sequenceName = "customId")
 	private int id;
 	@Column(name = "name")
 	private String name;
 	@Column(name = "country")
 	private String country;
+
+	public User() {
+
+	}
+
+	public User(String name, String country) {
+		this.name = name;
+		this.country = country;
+	}
 
 	public String getName() {
 		return name;
